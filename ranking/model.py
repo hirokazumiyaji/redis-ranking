580fa9023d18e64b86d23b8d589024cdafc7290f
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2013-10-25 22:32:15 +0900

    refactor

--- ranking/model.py
+++ ranking/model.py
@@ -1,27 +1,43 @@
 # coding: utf-8
-
+import logging
 import redis
 
 
+logger = logging.getLogger('redis_ranking')
+
+
 class Ranking(object):
+    EXPIRE = 60 * 60 * 24 * 14  # two week
 
-    def __init__(self, key, user_id, client=None, **kwargs):
+    def __init__(self, key, unique_id, client=None, **kwargs):
         self.key = key
-        self.user_id = user_id
+        self.unique_id = unique_id
         if client:
-            self.redis = client
+            self.client = client
         else:
-            self.redis = redis.StrictRedis(**kwargs)
+            self.client = redis.StrictRedis(**kwargs)
 
-    def __get_rank(self, redis=None):
-        if not redis:
-            redis = self.redis
-        return redis.zrevrank(self.key, self.user_id) + 1
+        score = kwargs.get('score', None)
+        if score:
+            self._score = score
 
-    def __get_score(self, redis=None):
-        if not redis:
-            redis = self.redis
-        return redis.zscore(self.key, self.user_id)
+        rank = kwargs.get('rank', None)
+        if rank:
+            self._rank = rank
+
+    def __get_rank(self):
+        score = self.score
+        count = self.client.zcount(self.key, score + 1, '+inf')
+        logger.debug('rank\tunique_id:{}\trank:{}'.format(self.unique_id,
+                                                          count + 1))
+        return count + 1
+
+    def __get_score(self):
+        score = self.client.zscore(self.key, self.unique_id)
+        self.expire()
+        logger.debug('score\tuniques_id:{}\tscore:{}'.format(self.unique_id,
+                                                             score))
+        return score
 
     @property
     def rank(self):
@@ -38,22 +54,38 @@ class Ranking(object):
         return self._score
 
     def add(self, score):
-        with self.redis.pipeline() as pipe:
+        with self.client.pipeline() as pipe:
             pipe.multi()
-            pipe.zadd(self.key, score, self.user_id)
-            self._rank = self.__get_rank(pipe)
-            self._score = self.__score(pipe)
-
+            pipe.zadd(self.key, score, self.unique_id)
+            self.expire(pipe)
             pipe.execute()
+
+        self._rank = self.__get_rank(pipe)
+        self._score = self.__score(pipe) 
 
     @classmethod
     def get_range(cls, key, begin, end, client=None, **kwargs):
         if not client:
             client = redis.StrictRedis(**kwargs)
-        return client.zrevrange(key, begin - 1, end - 1, withscores=True)
+
+        range_data = client.zrevrange(key, begin - 1, end -1, withscores=True)
+        if cls.EXPIRE:
+            client.expire(key, cls.EXPIRE)
+        return [cls(key, unique_id, client=client, score=score)
+                for unique_id, score in range_data]
 
     @classmethod
     def get_all(cls, key, client=None, **kwargs):
         if not client:
             client = redis.StrictRedis(**kwargs)
-        return client.zrevrange(key, 0, -1, withscores=True)
+
+        # zrevrange key, 0, -1
+        return cls.get_range(key, 1, 0, client=client, **kwargs)
+
+    def expire(self, client=None):
+        if not self.EXPIRE:
+            return
+
+        if not client:
+            client = self.client
+        client.expire(self.key, self.EXPIRE)
